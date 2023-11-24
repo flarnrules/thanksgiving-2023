50e19b8375be560a082525988e517d7d14e28d69
flarnrules <breifenberg11@gmail.com>
2023-11-23 19:46:17 -0600

    stuff

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
-<script type="text/javascript">
-    const correctPassword = 'YourPassword'; // Replace 'YourPassword' with your actual password
-    const userPassword = prompt("Please enter the password to access recipes:");
+document.addEventListener('DOMContentLoaded', () => {
+    const guestNames = ['Alison Reifenberg', 'Ben Reifenberg', 'Jean Reifenberg', 'Anne Rack', 'Tim Rack', 'Mary Gearing', 'Adam Gearing', 'Jake Tobol']; // Add your guest names here
+    const shuffledGuests = shuffleArray(guestNames);
+    const listContainer = document.getElementById('guest-list');
+    
+    shuffledGuests.forEach(name => {
+        const listItem = document.createElement('li');
+        listItem.textContent = name;
+        listContainer.appendChild(listItem);
+    });
+});
 
-    if(userPassword !== correctPassword) {
-        document.body.innerHTML = 'Access Denied'
-    };
-</script>
+// Function to shuffle an array
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+    }
+    return array;
+}
+
